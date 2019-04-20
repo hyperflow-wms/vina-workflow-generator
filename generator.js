97cd97548940d0183dfd870499318c4fb50303b2
Krzysztof Burkat <burkat731@gmail.com>
2019-04-20 14:48:24 +0200

    Added firing limit attribute

--- generator.js
+++ generator.js
@@ -6,6 +6,7 @@ function createTask(name, functionName, executable, args, ins, outs) {
         "name": name,
         "function": functionName,
         "type": "dataflow",
+		"firingLimit": 1,
         "config": {
             "executor": {
                 "executable": executable,
